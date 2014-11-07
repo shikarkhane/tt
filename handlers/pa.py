23538687afe0f40a5c1ab09c6785b3bd63025ae6
Nikhilm <shikarkhane@gmail.com>
2014-11-07 20:12:38 +0100

    pre-alpha

--- handlers/pa.py
+++ handlers/pa.py
@@ -5,12 +5,12 @@ import logging
 # Log everything, and send it to stderr.
 logging.basicConfig(filename=settings.DEBUG_LOG,level=logging.ERROR,format='%(asctime)s %(message)s')
 
-class FeedHandler(tornado.web.RequestHandler):
+class PreAlphaHandler(tornado.web.RequestHandler):
     '''
-    get user feed
+    this handler is short-lived for web-app purpose till APIs are developed and tested
     '''
     def get(self):
         try:
-            self.write("get feed")
+            self.render("pa_send.html")
         except Exception,e:
             logging.exception(e)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tornado.ioloop
 import settings
 from handlers.squeeze import LandingHandler
+from handlers.pa import PreAlphaHandler
 
 application = tornado.web.Application([
     (r"/", LandingHandler),
     (r"/subscribe/(\w+[\.]?\w+[@]\w+[\.]\w+)/", LandingHandler),
+    (r"/pre-alpha/1618/", PreAlphaHandler),
 ], debug=settings.DEBUG, static_path = settings.STATIC_PATH, template_path =  settings.TEMPLATE_PATH,
         cookie_secret=settings.COOKIE_SECRET)
 
