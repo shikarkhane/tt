aa86cd8fa9aaa3a106f4b57438542f4f81aa816d
Nikhilm <shikarkhane@gmail.com>
2014-11-09 11:50:14 +0100

    attribution and http lib for posting

---
+++ libs/http_utility.py
@@ -0,0 +1,25 @@
+import urllib2
+import json
+
+def http_get(url):
+    req = urllib2.Request(url)
+    req.get_method = lambda: 'DELETE'
+    out = urllib2.urlopen(req)
+    return out.read()
+def http_post(url, json_data):
+    req = urllib2.Request(url, json.dumps(json_data))
+    req.get_method = lambda: 'POST'
+    out = urllib2.urlopen(req)
+    return out.read()
+def http_delete(url):
+    req = urllib2.Request(url)
+    req.get_method = lambda: 'DELETE'
+    out = urllib2.urlopen(req)
+    return out.read()
+def http_call(url, data = None, method = 'GET'):
+    if method == 'GET':
+        return http_get(url)
+    if method == 'POST':
+        return http_post(url, data)
+    if method == 'DELETE':
+        return http_delete(url)
