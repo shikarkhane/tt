10be5deb12d8bcb17c7020c6fc786ad4c54e05a5
Nikhil <nikhil.shikarkhane@comeon.com>
2014-11-28 15:04:02 +0100

    user profile wip

--- db/_user.py
+++ db/_user.py
@@ -1,4 +1,6 @@
 import json
+from libs.keys_utility import user_profile_key
+from libs.shards_utility import Shard
 
 class Profile(object):
     def __init__(self, *args):
@@ -7,7 +9,7 @@ class Profile(object):
             self.set_by_key_value(*args)
         else:
             self.set_by_values(*args)
-    def set_by_values(self, user, verified, registration_date, ip_address, country, language):
+    def set_by_values(self, user, verified=False, registration_date=None, ip_address=None, country=None, language=None):
         self.user = user
         self.verified = verified
         self.registration_date = registration_date
@@ -23,3 +25,27 @@ class Profile(object):
         self.ip_address = v["ip_address"]
         self.country = v["country"]
         self.language = v["language"]
+class Value(object):
+    def __init__(self, verified, registration_date, ip_address, country, language):
+        self.verified = verified
+        self.registration_date = registration_date
+        self.ip_address = ip_address
+        self.country = country
+        self.language = language
+class Profile_Data():
+    def __init__(self, connection_pool):
+        self.r = Shard(connection_pool).get_server
+    def save(self, p):
+        '''save profile'''
+        k = user_profile_key(p.user)
+        val = Value(verified=p.verified, registration_date=p.registration_date, ip_address=p.ip_address,
+                    country=p.country, language=p.language)
+        if self.r(k).setnx( k,json.dumps(val.__dict__)):
+            return True
+        else:
+            return False
+    def get(self, user):
+        k = user_profile_key(user)
+        return self.get_by_key(k)
+    def get_by_key(self, k):
+        return Profile(k, self.r(k).get(k))
