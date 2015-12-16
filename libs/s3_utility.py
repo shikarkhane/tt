4244f6d65d7f52f2f25928cec8adb7a6941afbc5
Nikhil <nikhil.shikarkhane@comeon.com>
2015-12-16 15:28:01 +0100

    upload to s3 via cdn

--- libs/s3_utility.py
+++ libs/s3_utility.py
@@ -1,6 +1,16 @@
 import settings
 import boto
 from boto.s3.key import Key
+import json
+import os
+import base64
+import re
+import uuid
+import json
+import datetime
+import hmac
+import urllib
+import sha
 
 def save(bucketname, content, filename, content_type):
     conn = boto.connect_s3(settings.AWSAccessKeyId, settings.AWSSecretKey)
@@ -11,3 +21,44 @@ def save(bucketname, content, filename, content_type):
     x = k.set_contents_from_string(content)
     k.set_acl('public-read')
     return x
+
+
+def s3_upload_policy_document(bucketname):
+    """Generate policy based on
+    http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
+    """
+    b64policy = base64.b64encode(
+        json.dumps({
+            'expiration': (datetime.datetime.utcnow() + datetime.timedelta(minutes=30)).strftime('%Y-%m-%dT%H:%M:%S.000Z'),
+            'conditions': [
+                {'bucket': bucketname},
+                {'acl': 'public-read'},
+                ["starts-with", "$key", "/"],
+                {'success_action_status': '201'}
+            ]
+        })
+    )
+    return unicode(b64policy)
+
+
+def s3_upload_signature(bucketname):
+    """Generate signature based on
+    http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
+    """
+    signature = base64.encodestring(hmac.new(settings.AWSAccessKeyId, s3_upload_policy_document(bucketname), sha).digest()).strip()
+    return signature
+
+def signed_urls(bucketname, filename, filecontent):
+    title = filename
+    payload = {
+        'AWSAccessKeyId': settings.AWSAccessKeyId,
+        'policy': s3_upload_policy_document(bucketname),
+        'signature': s3_upload_signature(bucketname),
+        'key': "/%s/%s" % (str(uuid.uuid4()), title),
+        'success_action_redirect': "/",
+        'acl' : 'public-read',
+        'success_action_status' : '201',
+        'file' : filecontent
+    }
+    return payload
+
--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 import settings
 import boto
 from boto.s3.key import Key
+import requests
+from libs.s3_utility import signed_urls
 
-conn = boto.connect_s3(settings.AWSAccessKeyId, settings.AWSSecretKey)
-bucket = conn.get_bucket('tt-test-123', validate=True)
-k = Key(bucket)
-k.key = "image1.png"
-k.content_type = r.headers['content-type']
-k.set_contents_from_string(r.content)
+def save_to_s3():
+    conn = boto.connect_s3(settings.AWSAccessKeyId, settings.AWSSecretKey)
+    bucket = conn.get_bucket('tt-test-321', validate=True)
+    k = Key(bucket)
+    k.key = "what.txt"
+    k.content_type = 'text/plain'
+    with open( 'what.txt', 'r') as f:
+        content = f.read()
+    k.set_contents_from_string(content)
+    k.set_acl('public-read')
+
+def update_cdn():
+    with open( 'what.txt', 'r') as f:
+        content = f.read()
+    payload = signed_urls(bucketname='tt-test-321', filename='what.txt', filecontent=content)
+    r = requests.put("http://dn4s4tnuujqcy.cloudfront.net/what.txt", data=payload)
+    print r
+
+update_cdn()
+
