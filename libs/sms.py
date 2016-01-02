e5840ec4bf741a3865aab38094435f3b01b71151
Nikhilm <shikarkhane@gmail.com>
2016-01-02 11:56:49 +0100

    send welcome sms

--- libs/sms.py
+++ libs/sms.py
@@ -11,16 +11,20 @@ class twilio_provider():
         self.ACCOUNT_SID = settings.TWILIO_ACCOUNT_SID
         self.AUTH_TOKEN = settings.TWILIO_AUTH_TOKEN
         self.ACCOUNT_NUMBER= settings.TWILIO_ACCOUNT_NUMBER
-    def send_sms(self, to_user, code):
+    def send_code(self, to_user, code):
         client = TwilioRestClient(self.ACCOUNT_SID, self.AUTH_TOKEN)
         client.messages.create( to=to_user, from_=self.ACCOUNT_NUMBER, body="From Tinktime: Enter verification code:{0}".format(code))
+    def send_welcome(self, to_user):
+        client = TwilioRestClient(self.ACCOUNT_SID, self.AUTH_TOKEN)
+        client.messages.create( to=to_user, from_=self.ACCOUNT_NUMBER, body="Welcome to Tinktime! You registered successfully. Happy tinking.")
 
 def send_sms_verfication_code(connection_pool, user):
     code = get_sms_code()
     Verify(connection_pool).save_code(user, code)
-    twilio_provider().send_sms(user, code)
+    twilio_provider().send_code(user, code)
 def verify_sms_verfication_code(connection_pool, user, code):
     r = Verify(connection_pool).verify_code(user, code)
     if r:
         verified_by_sms_code(connection_pool, user)
+        twilio_provider().send_welcome(user)
     return r
