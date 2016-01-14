d49a55de54c7d213c14dd47dfc095e9bcd018956
Nikhil <nikhil.shikarkhane@comeon.com>
2016-01-14 17:06:31 +0100

    bug fix

--- libs/sms.py
+++ libs/sms.py
@@ -2,7 +2,7 @@ from db._sms import Verify
 from utility import get_sms_code
 from user import verified_by_sms_code
 import settings
-from libs import decision
+from libs.decision import send_welcome_message
 
 from twilio.rest import TwilioRestClient
 
@@ -27,6 +27,6 @@ def verify_sms_verfication_code(connection_pool, user, code):
     r = Verify(connection_pool).verify_code(user, code)
     if r:
         verified_by_sms_code(connection_pool, user)
-        decision.welcome_message(connection_pool, user)
+        send_welcome_message(connection_pool, user)
         twilio_provider().send_welcome(user)
     return r
