448b7f9329e532dee6da008b8925a0853e0c005d
Nikhil <nikhil.shikarkhane@comeon.com>
2016-01-19 11:06:47 +0100

    trinkets preview and styling fixed

--- static/js/trinket.js
+++ static/js/trinket.js
@@ -97,7 +97,7 @@ $(document).on('click', "button.deactivate", function(event) {
 
 $(document).on('click', "button.activate", function(event) {
     event.preventDefault();
-    var name = $(this).siblings('span.trinketname')[0].innerText;
+    var name = $(this).attr('data-trinketname');
     $.ajax({
                 type: 'POST',
             contentType: 'application/json',
@@ -109,15 +109,48 @@ $(document).on('click', "button.activate", function(event) {
 });
 $(document).on('click', "button.preview", function(event) {
     event.preventDefault();
-    var name = $(this).siblings('span.trinketname')[0].innerText;
-    document.getElementById('tinkcontainer-'+name).contentWindow.tt_start_animation();
+    var name = $(this).attr('data-trinketname'),
+    swiffyurl = $(this).attr('data-swiffyurl');
+    createPopup(name, swiffyurl);
 
+    window.setTimeout(function(){document.getElementById('tinkcontainer-'+name).contentWindow.tt_start_animation();}, 2000);
 });
-$(document).on('click', "button.closepreview", function(event) {
-    event.preventDefault();
-    var name = $(this).siblings('span.trinketname')[0].innerText;
-    document.getElementById('tinkcontainer-'+name).contentWindow.tt_stop_animation();
 
-});
-//Ext.getDom('tinkcontainer').contentWindow.tt_start_animation();
-//Ext.getDom('tinkcontainer').contentWindow.tt_stop_animation();
+function createPopup(tname, sUrl){
+
+    // close button
+    var closeBtn = $('<a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a>').button();
+
+    // text you get from Ajax
+    var content = '<iframe id="tinkcontainer-'+tname+'"  src="'+sUrl+'"></iframe>';
+
+    // Popup body - set width is optional - append button and Ajax msg
+    var popup = $("<div/>", {
+        "data-role": "popup"
+    }).css({
+        "width": $(window).width() / 1.5 + "px",
+        "height": $(window).height() / 1.5 + "px"
+    }).append(closeBtn).append(content);
+
+    // Append it to active page
+    $(".ui-page-active").append(popup);
+
+    // Create it and add listener to delete it once it's closed
+    // open it
+    $("[data-role=popup]").on("popupafterclose", function () {
+        $(this).remove();
+    }).on("popupafteropen", function () {
+        $(this).popup("reposition", {
+            "positionTo": "window"
+            //x: 150,
+            //y: 200
+        });
+    }).popup({
+        "dismissible": false,
+            "history": false,
+            "theme": "d",
+            "overlayTheme": "a"
+    }).popup("open");
+
+
+}
