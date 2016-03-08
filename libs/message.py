24ec42e18ab2161e6e4a059cc65ad89fcbd1f105
Nikhilm <shikarkhane@gmail.com>
2016-03-08 18:54:34 +0100

    Welome trinket

--- libs/message.py
+++ libs/message.py
@@ -5,6 +5,7 @@ from settings import COMMUNITY_MANAGER, CONVERSATION_LENGTH_LIMIT
 from libs.trinket import get_random_active_trinket
 import time
 import custom_text
+import settings
 
 def save_message(connection_pool, data):
     m = Message(data["from_user"], data["to_user"], data["send_timestamp"], data["trinket_name"], data["text"],
@@ -31,8 +32,8 @@ def decrement_unread_count_in_feed_summary(connection_pool, msg):
 
 def welcome_message(pool, user):
     tt_team = COMMUNITY_MANAGER['phone_number']
-    rand_trinket = get_random_active_trinket(pool)
+    welcome_trinket = settings.WELCOME_TRINKET_NAME
     welcome_text = custom_text.COMMUNICATE["Welcome"]
-    data = {"from_user": tt_team, "to_user" : user, "send_timestamp" : time.time()*1000 , "trinket_name" : rand_trinket,
+    data = {"from_user": tt_team, "to_user" : user, "send_timestamp" : time.time()*1000 , "trinket_name" : welcome_trinket,
             "text" : welcome_text, "seconds_sent": randint(1,9), "unread": True }
     save_message(pool, data)
