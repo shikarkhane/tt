d3a7d5406681bf72d03eb201924179e0beec9842
Nikhilm <shikarkhane@gmail.com>
2016-05-24 20:41:03 +0200

    log tink and error

--- handlers/message.py
+++ handlers/message.py
@@ -7,12 +7,9 @@ from libs.message import save_message, message_read
 from libs.push import generic
 import logstash
 
-ls_host = '10.0.0.169'
-ls_port = 5000
-
 ls_logger = logging.getLogger('python-logstash-logger')
 ls_logger.setLevel(logging.INFO)
-ls_logger.addHandler(logstash.LogstashHandler(ls_host, ls_port, version=1))
+ls_logger.addHandler(logstash.TCPLogstashHandler(settings.LOGSTASH_SERVER, settings.LOGSTASH_PORT, version=1))
 
 # Log everything, and send it to stderr.
 #logging.basicConfig(filename=settings.DEBUG_LOG,level=logging.ERROR,format='%(asctime)s %(message)s')
@@ -27,7 +24,7 @@ class MessageReadHandlerV2(tornado.web.RequestHandler):
             r = message_read(self.application.settings["db_connection_pool"], d)
             self.write("msg is read")
         except Exception,e:
-            ls_logger.error(e)
+            ls_logger.error(e, extra={'tt-type': 'error'})
 class MessageHandler(tornado.web.RequestHandler):
     '''
     messages sent, deleted, listed
@@ -37,22 +34,22 @@ class MessageHandler(tornado.web.RequestHandler):
         try:
             self.write("message get")
         except Exception,e:
-            ls_logger.error(e)
+            ls_logger.error(e, extra={'tt-type': 'error'})
     @gen.coroutine
     def post(self):
         try:
             d = json.loads(self.request.body)
             r = save_message(self.application.settings["db_connection_pool"], d)
             #self.write("writing msg to receiver feed")
-            ls_logger.info(json.dumps(d))
+            ls_logger.info('tink', extra={'tt-type': 'tink', 'tink': d})
         except Exception,e:
-            ls_logger.error(e)
+            ls_logger.error(e, extra={'tt-type': 'error'})
     @gen.coroutine
     def delete(self):
         try:
             self.write("message deleted")
         except Exception,e:
-            ls_logger.error(e)
+            ls_logger.error(e, extra={'tt-type': 'error'})
 class QueueWriter(tornado.web.RequestHandler):
     '''
     write message to queue
@@ -65,7 +62,7 @@ class QueueWriter(tornado.web.RequestHandler):
             http_call('/message-listener/', data, 'POST', True)
             #self.write("writing msg to queue")
         except Exception,e:
-            ls_logger.error(e)
+            ls_logger.error(e, extra={'tt-type': 'error'})
 class QueueListener(tornado.web.RequestHandler):
     '''
     methods here put the messages into receivers feed storage
@@ -80,4 +77,4 @@ class QueueListener(tornado.web.RequestHandler):
             generic(self.application.settings["db_connection_pool"], data["to_user"])
             #self.write("on call back from queue, will call messagehandler.post and push notify reciever phone")
         except Exception,e:
-            ls_logger.error(e)
+            ls_logger.error(e, extra={'tt-type': 'error'})
